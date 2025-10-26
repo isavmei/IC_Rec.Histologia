fc3cbaf38f36efbefc33b4c50c6051d019e7b6f0
isavmei <isabela.avelar@gmail.com>
2025-10-26 15:41:32 -0300

    alterações para comentários específicos dentro dos códigos e fontes :text:

--- teste_fase2.py
+++ teste_fase2.py
@@ -19,8 +19,6 @@ import hashlib
 import matplotlib.pyplot as plt
 import math
 
-# Configurações
-
 base_dir = '/content/dataset2-master/dataset2-master/images/TEST'
 saida_dir = '/content/dataset2-master/dataset2-master/images/VALIDAS'
 nova_resolucao = (256, 256)
@@ -28,28 +26,30 @@ novo_formato = 'JPEG'
 qualidade = 95
 lote_plot = 5  # imagens por figura
 
-# Criar pasta de saída se não existir
-os.makedirs(saida_dir, exist_ok=True)
+os.makedirs(saida_dir, exist_ok=True) #cria pastas da saída de imagens, que se existirem o código continua normalmente
 
-classes = [c for c in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, c))]
+classes = [tipos for tipos in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, tipos))]
+# cria uma pasta chamada classes, que tem somente dentro dela as pastas que tem sob a pasta principal.
 
+
+# -------------------------------
 # 1. Verificação de integridade
-
+# -------------------------------
 corrompidos = []
 
-for classe in classes:
-    caminho_classe = os.path.join(base_dir, classe)
-    for arquivo in os.listdir(caminho_classe):
-        caminho_arquivo = os.path.join(caminho_classe, arquivo)
+for classe in classes: #percorre cada pasta de classe dentro do dataset 
+    caminho_classe = os.path.join(base_dir, classe) #monta o caminho que vai ser encontrado da classe
+    for arquivo in os.listdir(caminho_classe):  #entra na classe e lista tudo o que tem dentro dela, arquivo por arquivo 
+        caminho_arquivo = os.path.join(caminho_classe, arquivo) #junta o caminho e o nome do arquivo pra abrir a imagem
         try:
-            with Image.open(caminho_arquivo) as img:
-                img.verify()
-        except:
-            corrompidos.append(caminho_arquivo)
+            with Image.open(caminho_arquivo) as img: #tenta abrir o arquivo como uma imagem pelo pillow e que vai ser automaticamente fechado depois
+                img.verify() #verifica se a imagem não está corrompida
+        except: #se for ocorrer erro, o código entra nesse bloco
+            corrompidos.append(caminho_arquivo) #o joga para a lista de corrompidos, para uma pasta.
 
-
+# -------------------------------
 # 2. Verificação de baixa resolução (<64x64)
-
+# -------------------------------
 baixa_res = []
 
 for classe in classes:
@@ -57,16 +57,17 @@ for classe in classes:
     for arquivo in os.listdir(caminho_classe):
         caminho_arquivo = os.path.join(caminho_classe, arquivo)
         if caminho_arquivo in corrompidos:
-            continue
+            continue #se estiver em corrompidos ele já pula 
         try:
-            with Image.open(caminho_arquivo) as img:
-                if img.width < 64 or img.height < 64:
-                    baixa_res.append(caminho_arquivo)
+            with Image.open(caminho_arquivo) as img: #usa pillow pra abrir a imagem que está na local caminho arquivo #with abre temporariamente e fecha automaticamente
+                if img.width < 64 or img.height < 64: #verifica o tamanho da imagem (resolução) 
+                    baixa_res.append(caminho_arquivo) #se for baixa resolução, adiciona seu caminho a lista de baixa resolução 
         except:
             continue
 
+# -------------------------------
 # 3. Detectar duplicatas exatas
-
+# -------------------------------
 hashes = {}
 duplicadas = []
 
@@ -75,18 +76,16 @@ for classe in classes:
     for arquivo in os.listdir(caminho_classe):
         caminho_arquivo = os.path.join(caminho_classe, arquivo)
         if caminho_arquivo in corrompidos or caminho_arquivo in baixa_res:
-            continue
-        try:
-            with open(caminho_arquivo, 'rb') as f:
-                h = hashlib.sha256(f.read()).hexdigest()
-            if h in hashes:
-                duplicadas.append(caminho_arquivo)
+            continue #ser for corrompido ou baixa resolução ele não vai entrar na busca já, diretamente
+        try: #tenta executar o codigo de leitura e o hash do arquivo 
+            with open(caminho_arquivo, 'rb') as f: #abre e le o arquivo como binário
+                h = hashlib.sha256(f.read()).hexdigest() #cria um hash do arquivo, calcula o hash SHA-256, uma “impressão digital” única para aquele conteúdo, e transforma em um hexadecimal legível 
+            if h in hashes: #verifica se o código já foi visto antes 
+                duplicadas.append(caminho_arquivo) #se já existe, é uma imagem duplicada da outra e vai ser adicionado ou excluido depois.
             else:
-                hashes[h] = caminho_arquivo
-        except:
+                hashes[h] = caminho_arquivo #se não foi visto é adicionado ao dicionario como novo item. guarda o hash e o caminho.
+        except: #se der errado entra aqui e passa pra próxima iteração do loop. 
             continue
-
-
 # 4. Padronizar imagens válidas e salvar
 
 validas = []
